62aebfd3738cade8611fc842bb6219f9e0fdf471
SHAO XUAN LEE <shaol@royal-22.cs.wisc.edu>
2023-12-05 20:11:03 -0600

    edited makefile, compiled

--- mount.wfs.c
+++ mount.wfs.c
@@ -6,11 +6,12 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 //parse the path into filename
-char* get_filename(char* path){
-    char* filename;
-    filename = path;
+char* get_filename(const char* path){
+    char* filename = 0;
+    strcpy(filename, path);
 
     while(*filename != '\0'){
         filename++;
@@ -29,30 +30,32 @@ static int wfs_getattr(const char *path, struct stat *stbuf) {
     // Fill stbuf structure with the attributes of the file/directory indicated by path
     // ...
     int res = 0;
-    char* filename = get_filename(path);
+    //char* filename = get_filename(path);
     memset(stbuf, 0, sizeof(struct stat));
-    if(strcmp(path, '/') == 0){
-        stbuf->st_uid = getuid;
-        stbuf->st_gid = getgid;
+    if(strcmp(path, "/") == 0){
+        stbuf->st_uid = (uid_t)getuid();
+        stbuf->st_gid = (uid_t)getgid();
         stbuf->st_mtime = time(NULL);
         stbuf->st_mode =  __S_IFDIR | 0755;
         stbuf->st_nlink = 2;
         return res;
     }
     else{
-        stbuf->st_uid = getuid;
-        stbuf->st_gid = getgid;
+        stbuf->st_uid = (uid_t)getuid();
+        stbuf->st_gid = (uid_t)getgid();
         stbuf->st_mtime = time(NULL);
         stbuf->st_mode =  __S_IFREG | 0755;
         stbuf->st_nlink = 1;
-        fseek(filename, 0L, SEEK_END);
-        int sz = ftell(filename);
-        stbuf->st_size = sz;
+        // fseek(filename, 0L, SEEK_END);
+        // int sz = ftell(filename);
+        // stbuf->st_size = sz;
     }
     return 0; // Return 0 on success
 }
 
-static int wfs_mknod(){}
+static int wfs_mknod(){
+    return 0;
+}
 
 static int wfs_mkdir(const char *path, mode_t mode){
 
@@ -63,14 +66,20 @@ static int wfs_mkdir(const char *path, mode_t mode){
 
     return 0;
 }
-static int wfs_read(){}
-static int wfs_write(){}
-static int wfs_readdir(){}
+static int wfs_read(){
+    return 0;
+}
+static int wfs_write(){
+    return 0;
+}
+static int wfs_readdir(){
+    return 0;
+}
 
 static int wfs_unlink(const char *path){
     int res;
     res = unlink(path);
-    if(res = -1)
+    if(res == -1)
         return -errno;
 
     return 0;
@@ -78,7 +87,7 @@ static int wfs_unlink(const char *path){
 
 
 static struct fuse_operations wfs_operations = {
-    .getattr = wfs_getattr,
+    .getattr    = wfs_getattr,
     .mknod      = wfs_mknod,
     .mkdir      = wfs_mkdir,
     .read	    = wfs_read,
